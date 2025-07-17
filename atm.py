919c53f1c649a63a4b022b554de7121ff1ec5f7f
kickcik <4778009@gmail.com>
2025-07-17 18:58:45 +0900

    ✨ 입금 기능 구현
    사용자가 입금 금액을 입력하고, 잔액을 갱신하는 기능 추가

--- atm.py
+++ atm.py
@@ -1,15 +1,22 @@
 balance = 10000
 
-while (num := input('기능 선택| 1.입금, 2.출금, 3.영수증 보기, 4. 종료')) != '4':
-    if num == '1':
-        pass
+while (num := input('기능 선택| 1.입금, 2.출금, 3.영수증 보기, 4. 종료 : ')) != '4':
+    if num == '1': # 입금 기능 구현 feat/deposit
+        try:
+            balance += (deposit_amount := int(input('입금할 급액을 입력해주세요: ')))
+            print(f'입금하신 금액: {deposit_amount:,}원')
+            print(f'현재 잔액 : {balance:,}원')
+        except Exception as e:
+            print(f'예외 발생 : {type(e)}')
+            print(f'내용 : {e}')
 
     if num == '2':
         pass
-    
+
     if num == '3':
         pass
 
+print(f'서비스가 종료되었습니다.')
 print(f'현재 잔액 : {balance:,}원')
 
 
