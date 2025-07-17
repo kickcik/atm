9d530faa313b0648a66e3b5e8bc586afcf97e600
kickcik <4778009@gmail.com>
2025-07-17 18:44:16 +0900

    ✨ 선택 가능한 번호 추가
    입금(1), 출금(2), 영수증 보기(3) 선택 가능한 조건 작성

--- atm.py
+++ atm.py
@@ -1,8 +1,15 @@
 balance = 10000
 
 while (num := input('기능 선택| 1.입금, 2.출금, 3.영수증 보기, 4. 종료')) != '4':
-    if num == 1:
+    if num == '1':
         pass
+
+    if num == '2':
+        pass
+    
+    if num == '3':
+        pass
+
 print(f'현재 잔액 : {balance:,}원')
 
 
